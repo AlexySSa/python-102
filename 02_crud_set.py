f6c691af7f8cb705997b38bb2511064f9e4c18f0
Alexy <aassuriano@gmail.com>
2023-01-07 02:49:46 +0000

    Operaciones con conjuntos

--- 02_crud_set.py
+++ 02_crud_set.py
@@ -15,3 +15,19 @@ print(set_contries)
 #update
 set_contries.update({'argentina', 'chile', 'peru'})
 print(set_contries)
+
+#remove
+
+set_contries.remove('colombia')
+print(set_contries)
+
+set_contries.remove('argentina')
+#discard sirve igual que el remove pero no tira ningun error durante el proceso
+set_contries.discard('argentinas')
+print(set_contries)
+
+set_contries.add('argentina')
+print(set_contries)
+
+set_contries.clear()
+print(len(set_contries))

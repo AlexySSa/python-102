9ed102d3cc78dc5a69e4c445cc8a5941df3d6791
Alexy <aassuriano@gmail.com>
2023-01-10 03:41:49 +0000

    pues que se me olvidan como van las funciones por rato pero ahí vamos

---
+++ acordarme_de_las_funciones.py
@@ -0,0 +1,14 @@
+import random
+def message_creator(text):
+   if text == 'computadora':
+      return 'Con mi computadora puedo programar usando Python'
+   elif text == 'celular':
+      return 'En mi celular puedo aprender usando la app de Platzi'
+   elif text == 'cable':
+      return '¡Hay un cable en mi bota!'
+   else:
+      return 'Artículo no encontrado'
+
+text = random.choice(['computadora', 'celular', 'cable'])
+response = message_creator(text)
+print(response)

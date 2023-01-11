c0c9df8f3957263a41a17bf16c2fa4b62cc373ea
Alexy <aassuriano@gmail.com>
2023-01-11 01:32:25 +0000

    una función dentro de otra función

---
+++ 13_hof.py
@@ -0,0 +1,26 @@
+def increment(x):
+  return x + 1
+
+incremet_2 = lambda x : x + 1
+
+
+def hof(x, func):
+  return x + func(x)
+
+hof_2 = lambda x, func: x + func(x)
+
+result = hof(2, increment)
+# 2 + (2 + 1)
+print(result)
+
+
+
+result = hof_2(2, incremet_2)
+# 2 + (2 + 1)
+print(result)
+
+result = hof_2(2, lambda x : x + 2)
+print(result)
+
+result = hof_2(2, lambda x : x * 5)
+print(result)

14e1671d01c476a219666dfe6cedce1378de3f04
Alexy <aassuriano@gmail.com>
2023-01-12 01:31:33 +0000

    usar map con listas

--- 13_hof.py
+++ 13_hof.py
@@ -1,19 +1,19 @@
 def increment(x):
   return x + 1
 
-incremet_2 = lambda x : x + 1
-
-
 def hof(x, func):
   return x + func(x)
-
-hof_2 = lambda x, func: x + func(x)
 
 result = hof(2, increment)
 # 2 + (2 + 1)
 print(result)
 
 
+#con lambdas
+
+incremet_2 = lambda x : x + 1
+
+hof_2 = lambda x, func: x + func(x)
 
 result = hof_2(2, incremet_2)
 # 2 + (2 + 1)
